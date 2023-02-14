be552c7a2bab59b7209e8588b749b5cf948c20f0
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-02-14 12:33:46 +0100

    Made a "model". Tested "model" insertion into the database.

--- my_site/office/models.py
+++ my_site/office/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    age = models.IntegerField()
